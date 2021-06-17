b0c338a9e1e29875e0a830ca68ce7ab99b116b7f
Artemiy Kononov <artemiy.kononov@jetbrains.com>
2021-06-17 20:30:38 +0300

    kek

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-import telebot
+import json
 import sqlite3
+from string import Template
+from urllib.parse import quote_plus
+from urllib.request import urlopen
+import telebot
 from telebot import types
 
-bot = telebot.TeleBot("1854969870:AAEtLeBVWBQuZaQANzCA9n1PE2jiqgR_Yu4")
+from util import *
+
+bot = telebot.TeleBot(telegram_api_key_string)
 
 
-@bot.message_handler(commands=['start'])
-def begin(message, text="Здравствуй. Что будем делать?"):
+@bot.message_handler(commands=["start"])
+def begin(message, text=welcome_string):
     keyboard = types.ReplyKeyboardMarkup(row_width=3)
-    k1 = types.KeyboardButton('Добавить товар в список')
-    k2 = types.KeyboardButton('Показать список')
-    k3 = types.KeyboardButton('Посчитать общую стоимость')
-    k4 = types.KeyboardButton('Убрать одну покупку')
-    k5 = types.KeyboardButton('Очистить список')
-    k6 = types.KeyboardButton('Достаточно')
-    k7 = types.KeyboardButton('свободная кнопка')
-    k8 = types.KeyboardButton('свободная кнопка')
-    keyboard.add(k1, k2, k3)
-    keyboard.add(k4, k5, k6)
-    keyboard.add(k7, k8)
+
+    add_item_button = types.KeyboardButton(add_item_button_string)
+    show_items_button = types.KeyboardButton(show_items_button_string)
+    calculate_items_button = types.KeyboardButton(calculate_items_button_string)
+    remove_item_button = types.KeyboardButton(remove_item_button_string)
+    clear_items_button = types.KeyboardButton(clear_items_button_string)
+    finish_button = types.KeyboardButton(finish_button_string)
+    show_weather_button = types.KeyboardButton(show_weather_button_string)
+    analyze_stock_button = types.KeyboardButton(analyze_stock_button_string)
+
+    keyboard.add(add_item_button, show_items_button, calculate_items_button)
+    keyboard.add(remove_item_button, clear_items_button, finish_button)
+    keyboard.add(show_weather_button, analyze_stock_button)
+
     msg = bot.send_message(message.from_user.id, text=text, reply_markup=keyboard)
 
     bot.register_next_step_handler(msg, answer)
-    a = sqlite3.connect('list.db')
-    cursor = a.cursor()
+
+    cursor = sqlite3.connect(list_bd_key_string).cursor()
 
     try:
         check = "CREATE TABLE \"list\" (\"ID\" INTEGER UNIQUE, \"user_id\" INTEGER, \"item\" TEXT, PRIMARY KEY (\"ID\"))"
@@ -32,103 +41,149 @@ def begin(message, text="Здравствуй. Что будем делать?")
         pass
 
 
-def aditem(a):
-    with sqlite3.connect('list.db') as i:
-        cursor = i.cursor()
-        cursor.execute('INSERT INTO list (user_id, item) VALUES (?, ?)', (a.from_user.id, a.text))
-        i.commit()
-    begin(a, "Добавил. Что ещё сделаем?")
+def add_item(message):
+    text = message.text
+    text = text.split()
+    try:
+        float(text[-1])
+    except:
+        begin(message, "Введи цену последней!")
+        return
+
+    with sqlite3.connect(list_bd_key_string) as connection:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO list (user_id, item) VALUES (?, ?)", (message.from_user.id, message.text))
+        connection.commit()
+    begin(message, added_string + help_string)
 
 
-def showlist(msg):
-    with sqlite3.connect('list.db') as i:
-        cursor = i.cursor()
-        cursor.execute('SELECT item FROM list WHERE user_id=={}'.format(msg.from_user.id))
-        a = cursor.fetchall()
-        alist = []
-        b = 1
-        for q in list(enumerate(a)):
-            alist.append(str(b) + '. ' + q[1][0] + '\n')
-            b += 1
-        a = ''.join(alist)
-        bot.send_message(msg.chat.id, a)
-        begin(msg, "Чем еще могу помочь?")
+def show_list(message):
+    with sqlite3.connect(list_bd_key_string) as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT item FROM list WHERE user_id=={}".format(message.from_user.id))
+        data = cursor.fetchall()
+        data_list = []
+        amount = 0
+        for item in list(data):
+            data_list.append(str(amount + 1) + ". " + item[0] + "\n")
+            amount += 1
+        data = "".join(data_list)
+        bot.send_message(message.chat.id, "Товаров: " + str(amount))
+        bot.send_message(message.chat.id, data)
+        begin(message, help_string)
 
 
-def count(msg):
-    with sqlite3.connect('list.db') as i:
-        cursor = i.cursor()
-        cursor.execute('SELECT item FROM list WHERE user_id=={}'.format(msg.from_user.id))
-        a = cursor.fetchall()
-        b = 0
-        for q in a:
-            q = q[-1]
-            q = q.split()
-            b += float(q[1])
-        bot.send_message(msg.chat.id, 'Итого набрано товаров на сумму: ' + str(b))
-        begin(msg, "Чем еще могу помочь?")
+def count(message):
+    with sqlite3.connect(list_bd_key_string) as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT item FROM list WHERE user_id=={}".format(message.from_user.id))
+        data = cursor.fetchall()
+        amount = 0
+        for item in list(data):
+            item = item[0].split()
+            amount += float(item[-1])
+        bot.send_message(message.chat.id, "Итого набрано товаров на сумму: " + str(amount))
+        begin(message, help_string)
 
 
-def delitem(msg):
-    delist = types.ReplyKeyboardMarkup(row_width=3)
-    with sqlite3.connect('list.db') as i:
-        cursor = i.cursor()
-        cursor.execute('SELECT item FROM list WHERE user_id=={}'.format(msg.from_user.id))
-        a = cursor.fetchall()
-        for q in a:
-            delist.add(types.KeyboardButton(q[0]))
-        msg = bot.send_message(msg.from_user.id, text="Какой товар убрать?", reply_markup=delist)
-        cursor.execute('DELETE FROM list WHERE user_id==? AND item==?', (msg.from_user.id, msg.text))
-        begin(msg, "Удалил. Что-то ещё сделаем?")
+def delete_item_start(message):
+    to_delete_list = types.ReplyKeyboardMarkup(row_width=3)
+    with sqlite3.connect(list_bd_key_string) as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT item FROM list WHERE user_id=={}".format(message.from_user.id))
+        data = cursor.fetchall()
+        for item in list(data):
+            to_delete_list.add(types.KeyboardButton(item[0]))
+        if len(list(data)) == 0:
+            begin(message, list_already_empty_string)
+            return
+        message = bot.send_message(message.from_user.id, text="Какой товар убрать?", reply_markup=to_delete_list)
+        bot.register_next_step_handler(message, delete_item_end)
 
 
-def clist(msg):
-    with sqlite3.connect('planner_hse.db') as i:
-        cursor = i.cursor()
-        cursor.execute('DELETE FROM list WHERE user_id=={}'.format(msg.from_user.id))
-        i.commit()
-    begin(msg, "Всё потёр. Что-то ещё сделаем?")
+def delete_item_end(message):
+    with sqlite3.connect(list_bd_key_string) as connection:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM list WHERE user_id==? AND item==?", (message.from_user.id, message.text))
+        connection.commit()
+        begin(message, removed_string + help_string)
 
 
-def answer(a):
-    if a.text == "Добавить товар в список":
-        msg = bot.send_message(a.chat.id,
-                               'Запиши, что хочешь добавить, в формате "название цена (без знакак валюты)". Пример: "Хлеб 20"')
-        bot.register_next_step_handler(msg, aditem)
-    if a.text == "Показать список":
+def clear_list(msg):
+    with sqlite3.connect(list_bd_key_string) as connection:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM list WHERE user_id=={}".format(msg.from_user.id))
+        connection.commit()
+    begin(msg, cleared_string + help_string)
+
+
+def evaluate_json(json_data):
+    temp = json_data["main"]["temp"]
+    feels_like = json_data["main"]["feels_like"]
+    return "Нынешняя: " + str(temp) + "С°. Ощущается как: " + str(feels_like) + "C°."
+
+
+def get_weather(message):
+    try:
+        interpolated = Template(weathermap_query_string).substitute(city=quote_plus(message.text),
+                                                                    units=default_units_key_string,
+                                                                    lang=default_lang_key_string,
+                                                                    api=weathermap_api_key_string)
+        response = urlopen(interpolated)
+        data = json.loads(response.read())
+        evaluated = evaluate_json(data)
+        begin(message, str(evaluated))
+    except:
+        begin(message, weather_error_string)
+
+
+def analyze_stock(message):
+    begin(message, "work in progress")
+
+
+def answer(message):
+    if message.text == add_item_button_string:
+        item = bot.send_message(message.chat.id, add_item_example_string)
+        bot.register_next_step_handler(item, add_item)
+    elif message.text == show_items_button_string:
         try:
-            showlist(a)
+            show_list(message)
         except:
-            bot.send_message(a.chat.id, 'Список пуст')
-            begin(a, "Что-нибудь ещё?")
-    if a.text == "Посчитать общую стоимость":
+            bot.send_message(message.chat.id, list_empty_string)
+            begin(message, begin_string)
+    elif message.text == calculate_items_button_string:
         try:
-            count(a)
+            count(message)
         except:
-            bot.send_message(a.chat.id, 'Список пуст')
-            begin(a, "Что-нибудь ещё?")
-    if a.text == "Убрать одну покупку":
+            bot.send_message(message.chat.id, list_empty_string)
+            begin(message, begin_string)
+    elif message.text == remove_item_button_string:
         try:
-            delitem(a)
+            delete_item_start(message)
         except:
-            bot.send_message(a.chat.id, 'Список уже пуст')
-            begin(a, "Что-нибудь ещё?")
-    if a.text == "Очистить список":
+            bot.send_message(message.chat.id, list_already_empty_string)
+            begin(message, begin_string)
+    elif message.text == clear_items_button_string:
         try:
-            clist(a)
+            clear_list(message)
         except:
-            bot.send_message(a.chat.id, 'Список уже пуст')
-            begin(a, "Что-нибудь ещё?")
-    if a.text == "свободная кнопка":
-        bot.send_message(a.chat.id, 'Коллеги, тут сделайте свою функцию')
-        begin(a, "Что-нибудь ещё?")
-    if a.text == "Достаточно":
-        bot.send_message(a.chat.id, 'Тогда прощаюсь до следующей команды /start')
+            bot.send_message(message.chat.id, list_already_empty_string)
+            begin(message, begin_string)
+    elif message.text == show_weather_button_string:
+        city = bot.send_message(message.chat.id, show_weather_example_string)
+        bot.register_next_step_handler(city, get_weather)
+    elif message.text == analyze_stock_button_string:
+        stock = bot.send_message(message.chat.id, show_stock_message_string)
+        bot.register_next_step_handler(stock, analyze_stock)
+    elif message.text == finish_button_string:
+        bot.send_message(message.chat.id, goodbye_string)
+    else:
+        begin(message.chat.id, dont_know_message_string)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=["text"])
 def handle_docs_audio(message):
-    begin(message, text="Моё сознание не охватывает данные сферы бытия. Попробуй спользовать кнопки")
+    bot.send_message(message.chat.id, text=error_string)
 
 
 bot.infinity_polling()
---
+++ util.py
@@ -0,0 +1,42 @@
+# secret
+telegram_api_key_string = "1854969870:AAEtLeBVWBQuZaQANzCA9n1PE2jiqgR_Yu4"
+weathermap_api_key_string = "5524835e0faa42da1b800b92df16079b"
+
+# weather query
+weathermap_query_string = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=$units&lang=$lang&appid=$api"
+default_lang_key_string = "ru"
+default_city_key_string = "moscow"
+default_units_key_string = "metric"
+
+# buttons
+add_item_button_string = "Добавить товар в список"
+show_items_button_string = "Показать список"
+calculate_items_button_string = "Посчитать общую стоимость"
+remove_item_button_string = "Убрать одну покупку"
+clear_items_button_string = "Очистить список"
+finish_button_string = "Достаточно"
+show_weather_button_string = "Узнать нынешнюю погоду"
+analyze_stock_button_string = "Анализировать акцию"
+
+# communication
+show_weather_example_string = "Введите город, например, Москва."
+welcome_string = "Здравствуй. Что будем делать?"
+
+add_item_example_string = "Запиши, что хочешь добавить, в формате 'название цена (без знакак валюты)'. Пример: 'Хлеб " \
+                          "20' "
+begin_string = "Что-нибудь ещё?"
+list_empty_string = "Список пуст."
+list_already_empty_string = "Список уже пуст."
+help_string = "Чем еще могу помочь?"
+added_string = "Добавил."
+removed_string = "Удалил."
+cleared_string = "Всё потёр."
+error_string = "Моё сознание не охватывает данные сферы бытия. Попробуй спользовать кнопки. Если ещё не начал - " \
+               "напиши /start. "
+goodbye_string = "Тогда прощаюсь до следующей команды /start."
+weather_error_string = "Не знаю такого города, попробуй другой."
+show_stock_message_string = "Напиши название акции или валюты(тикер с Yahoo finance, поанализирую.)"
+dont_know_message_string = "Я не знаю такую команду"
+
+# bd
+list_bd_key_string = "list.db"
